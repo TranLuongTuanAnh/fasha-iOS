9a9785554c763805d86f1c4961a17176b62f8e01
TranLuongTuanAnh <tlta.bkhn@gmail.com>
2017-09-25 00:00:09 +0900

    login api ok

--- fasha-iOS/Fasha/Controllers/UserLoginController.swift
+++ fasha-iOS/Fasha/Controllers/UserLoginController.swift
@@ -16,12 +16,24 @@ class UserLoginController: UIViewController {
     @IBOutlet weak var email: UITextField!
     @IBOutlet weak var password: UITextField!
     @IBOutlet weak var loginBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         combineLatest(email.reactive.text, password.reactive.text) { emailString , passwordString in
              return (emailString! as NSString).length > 0 && (passwordString! as NSString).length > 0
             }.bind(to: loginBtn.reactive.isEnabled)
+        
+        LoginState.shareInstance.transitionState.observeNext { state in
+            switch(state) {
+            case .success:
+                let homeVC = UIStoryboard(name:"Home", bundle:nil).instantiateInitialViewController()
+                self.navigationController?.pushViewController(homeVC!, animated: true)
+                LoginState.shareInstance.transitionState.next(.none)
+            default:
+                break
+            }
+        }.dispose(in: reactive.bag)
     }
     @IBAction func tapLogin(_ sender: Any) {
         let userService = UserService()
